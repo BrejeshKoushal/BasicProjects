567fcfdb4a0af8097a6adc607576579f1fa2b442
BrejeshKoushal <brejeshkoushal28@gmail.com>
2024-10-02 18:02:00 +0530

    ookkk

--- 06_projects/BMICalculator/javascript.js
+++ 06_projects/BMICalculator/javascript.js
@@ -1,9 +1,9 @@
 const form = document.querySelector('form');
 const results = document.querySelector('#results');
-
+const kyaHai = document.querySelector('#kya-hai');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    
+
     const height = parseInt(document.querySelector('#height').value);
     const weight = parseInt(document.querySelector('#weight').value);
 
@@ -11,14 +11,37 @@ form.addEventListener('submit', (e) => {
         results.innerHTML = 'Please enter a valid height in cm';
         return;
     }
-    
+
     if (isNaN(weight) || weight <= 0) {
         results.innerHTML = 'Please enter a valid weight in kg';
         return;
     }
 
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-
-
     results.innerHTML = `<span>Your BMI is: ${bmi}</span>`;
+
+    kyaHai.innerHTML = '';
+
+    if (bmi < 18.6) {
+        kyaHai.innerHTML = `
+            <div>Under-weight</div>
+            <div>Under-weight</div>
+            <div>Under-weight</div>
+            <div>Under-weight</div>
+        `;
+    } else if (bmi > 24.9) {
+        kyaHai.innerHTML = `
+            <div>Over-weight</div>
+            <div>Over-weight</div>
+            <div>Over-weight</div>
+            <div>Over-weight</div>
+        `;
+    } else {
+        kyaHai.innerHTML = `
+            <div>Perfect cutie</div>
+            <div>Perfect cutie</div>
+            <div>Perfect cutie</div>
+            <div>Perfect cutie</div>
+        `;
+    }
 });
